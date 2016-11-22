52bf88e4caebca88ee2789195160f7aaa402b69a
hyunju1117 <hyunju11117@gmail.com>
2016-11-22 19:25:26 +0900

    textview

--- app/src/main/java/org/androidtown/actionbar/day.java
+++ app/src/main/java/org/androidtown/actionbar/day.java
@@ -12,15 +12,10 @@ public class day extends Fragment {
     public day() {
         // Required empty public constructor
     }
-
-
-
-    @Override
+  @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_day, container, false);
     }
-
-
 }
